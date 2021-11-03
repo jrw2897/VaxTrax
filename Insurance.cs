94bd465cd6bfc77de4e36ca6159ef535dd89b9c6
jrw2897 <93672306+jrw2897@users.noreply.github.com>
2021-11-03 12:05:05 -0500

    Create Insurance.cs

---
+++ Insurance.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaxTrax_2._0_
+{
+    class Insurance
+    {
+        public string Insurer { get; set; }
+        public string Holder { get; set; }
+        public string GroupID { get; set; }
+
+    }
+}
