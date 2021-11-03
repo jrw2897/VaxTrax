3acaff06bc9d8ca3be2bd1eb1a41527771ccb6d2
jrw2897 <93672306+jrw2897@users.noreply.github.com>
2021-11-03 12:04:16 -0500

    Create DataGridVaccine.cs

---
+++ DataGridVaccine.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaxTrax_2._0_
+{
+    class DataGridVaccine
+    {
+        public int NumDose { get; set; }
+        public string Product { get; set; }
+        public string DateAdministered { get; set; }
+    }
+}
