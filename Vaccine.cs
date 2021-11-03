b7c887ac9d2cc346df64ea3dc0e9e0567220bea5
jrw2897 <93672306+jrw2897@users.noreply.github.com>
2021-11-03 12:08:40 -0500

    Create Vaccine.cs

---
+++ Vaccine.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaxTrax_2._0_
+{
+    class Vaccine
+    {
+        public int patient_ID { get; set; }
+        public int NumDose   { get; set; }
+        public string Vaccinator_Name { get; set; }
+        public string Administration_site { get; set; }
+        public string Product { get; set; }
+        public string Manufacture { get; set; }
+        public string Typecvx { get; set; }
+        public string LotNum { get; set; }
+        public string ExpirationDate { get; set; }
+        public string DateAdministered { get; set; }
+        public int NumWasted { get; set; }
+        public string MissedAppointment { get; set; }
+        public string Comorbidity { get; set; }
+        public string RevievedEUA { get; set; }
+    }
+}
