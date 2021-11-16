ae677ed3f73ee007959e55eabf4a0a82309d0fb5
jrw2897 <93672306+jrw2897@users.noreply.github.com>
2021-11-16 17:17:08 -0600

    Update Insurance.cs

--- Insurance.cs
+++ Insurance.cs
@@ -8,9 +8,10 @@ namespace VaxTrax_2._0_
 {
     class Insurance
     {
+        public int patient_ID { get; set; }
         public string Insurer { get; set; }
-        public string Holder { get; set; }
-        public string GroupID { get; set; }
+        public string Primary_Holder { get; set; }
+        public string Group_ID { get; set; }
 
     }
 }
