2637b9c146bebbc08c508e9e2ac422cded62d4f2
jrw2897 <93672306+jrw2897@users.noreply.github.com>
2021-12-07 16:31:42 -0600

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaxTrax_2._0_
+{
+    class User
+    {
+        public int User_ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
